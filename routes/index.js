3b029ace326ceed4f67055382524f12de518e1ac
bemery2 <31778170+bemery2@users.noreply.github.com>
2019-12-07 22:43:16 -0600

    small update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 
-var passport = require('passport-local')
+var passport = require('passport')
 , LocalStrategy = require('passport-local').Strategy;
 
 passport.use(new LocalStrategy(
